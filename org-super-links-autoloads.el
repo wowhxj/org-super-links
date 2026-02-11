b7d4f84848a8c13c14e57a0ea1b480e7294bcd6b
Randolph HUANG <xiaojianghuang@yahoo.com>
2026-02-11 15:35:59 +0800

    chore: add autoloads file for ELPA compatibility
    Add org-super-links-autoloads.el to prevent
    "Cannot open load file: org-super-links-autoloads" errors
    when loading the package via ELPA or package.el.

---
+++ org-super-links-autoloads.el
@@ -0,0 +1,34 @@
+;;; org-super-links-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "org-super-links" "org-super-links.el" (0 0 0 0))
+;;; Generated autoloads from org-super-links.el
+
+(autoload 'org-super-links-convert-link-to-super "org-super-links" "Convert a normal `org-mode' link at `point' to super link..." t nil)
+
+(autoload 'org-super-links-delete-link "org-super-links" "Delete the link at point, and the corresponding reverse link..." t nil)
+
+(autoload 'org-super-links-store-link "org-super-links" "Store a point to the register for use..." t nil)
+
+(autoload 'org-super-links-insert-link "org-super-links" "Insert a super link from the register..." t nil)
+
+(autoload 'org-super-links-link "org-super-links" "Insert a link and add a backlink..." t nil)
+
+(autoload 'org-super-links-quick-insert-drawer-link "org-super-links" "Insert link into drawer..." t nil)
+
+(autoload 'org-super-links-quick-insert-inline-link "org-super-links" "Insert inline link..." t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("org-super-links-pkg.el") (0 0 0 0))
+
+;;;***
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; org-super-links-autoloads.el ends here
